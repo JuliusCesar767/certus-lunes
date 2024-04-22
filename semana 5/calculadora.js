4b65435824b3a8648cb753fa048a7da3e95cb2ec
JuliusC777 <juliocesaruh8@gmail.com>
2024-04-22 11:08:35 -0500

    Java

---
+++ semana 5/calculadora.js
@@ -0,0 +1,16 @@
+function sumar(value1,value2){
+    var result1 = value1+value2;
+    return result1;
+}
+function rstar(value1,value2){
+    var result1 = value1+value2;
+    return result1;
+}
+function multiplicar(value1,value2){
+    var result1 = value1+value2;
+    return result1;
+}
+function dividir(value1,value2){
+    var result1 = value1+value2;
+    return result1;
+}
---
+++ semana 5/index.js
@@ -0,0 +1,52 @@
+ /* Mi primer código en Java Script 
+window.alert("Bienvenido a la página");
+*/
+var number_1 = 10;
+var number_2 = 12;
+var result   = 0;
+
+/* Operadores Aritméticos y Expresiones */
+result = number_1 + number_2;
+console.log(result);
+
+result = number_1 - number_2;
+console.log(result);
+
+result = number_1 / number_2;
+console.log(result);
+
+result = number_1 * number_2;
+console.log(result);
+document.getElementById("resultado").innerHTML="El resultado es: "+result;
+document.write("<h1>Certus oficial</h1>")
+var mensaje="";
+text="Inscribete"
+document.getElementById(mensaje).innerHTML=text;
+
+var cond1 = true;
+var cond2
+
+var edades = ("Juan","Pedro","Maria");
+var nombres = [19,11,22];
+var datos = nombres[0]+" "+edades[0];
+var datos = nombres[1]+" "+edades[1];
+var datos = nombres[2]+" "+edades[2];
+
+console.log(datos1);
+console.log(datos2);
+console.log(datos3);
+
+function sumar(value1,value2){
+    var result1 = value1+value2;
+    document.getElementById("function").innerHTML= "el resultado de la suma es:" +result
+}
+function sumar2(value1,value2){
+    var result1 = value1+value2;
+    return result1;
+}
+    document.getElementById ("function").innerHTML= "el resultado es: " +sumar2(1,3);
+
+
+sumar(1,2);
+var nombre= getElementById("my name");
+console.log(nombre);
